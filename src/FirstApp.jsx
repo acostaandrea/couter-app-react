60d38e45811f659adf5c465923616aa5cfd2cd0d
Andrea <acostaandreadev@gmail.com>
2025-04-10 13:18:23 -0300

    impresión de variables

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,7 +1,13 @@
+const newMesagge = {
+  message: 'Hello World',
+  title: 'My First App',
+}
+const mensaje = () => 'Hola mundo desde una función';
 export const FirstApp = () => {
   return (
     <>
-      <h1>Hola</h1>
+      <h1>{mensaje()}</h1>
+      {/* <code>{JSON.stringify(newMesagge)}</code> */}
     </>
   )
 }
